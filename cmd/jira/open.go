9f138e3b8345c7f059dfeebf7ebcb59ef2d6ad2b
Ankit Pokhrel <ankitpokhrel@gmail.com>
2020-12-07 15:54:29 +0100

    feat: Let open cmd open project page by default

--- cmd/jira/open.go
+++ cmd/jira/open.go
@@ -10,15 +10,22 @@ import (
 
 var openCmd = &cobra.Command{
 	Use:     "open [ISSUE KEY]",
-	Short:   "Open issue in a browser.",
-	Long:    `Open opens the issue in a browser.`,
-	Args:    cobra.MinimumNArgs(1),
+	Short:   "Open issue in a browser",
+	Long:    "Open opens issue in a browser. If the issue key is not given, it will open the project page.",
 	Aliases: []string{"browse", "navigate"},
 	Run:     open,
 }
 
 func open(_ *cobra.Command, args []string) {
-	url := fmt.Sprintf("%s/browse/%s", viper.Get("server"), args[0])
+	server := viper.GetString("server")
+
+	var url string
+
+	if len(args) == 0 {
+		url = fmt.Sprintf("%s/browse/%s", server, viper.GetString("project"))
+	} else {
+		url = fmt.Sprintf("%s/browse/%s", server, args[0])
+	}
 
 	exitIfError(browser.OpenURL(url))
 }
