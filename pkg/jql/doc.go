11418ad509b667ee949528ed256c6602dfb03294
Ankit Pokhrel <ankitpokhrel@gmail.com>
2020-12-11 19:46:17 +0100

    chore: Add JQL package doc

---
+++ pkg/jql/doc.go
@@ -0,0 +1,7 @@
+// Package jql is a very simple JQL query builder that cannot do a lot at the moment.
+//
+// There is no JQL syntax check and relies on the package user to construct a valid query.
+//
+// It cannot combine AND and OR query currently. That means you cannot construct a query like the one below:
+// 	project="JQL" AND issue in openSprints() AND (type="Story" OR resolution="Done")
+package jql
