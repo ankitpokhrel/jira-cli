e9b459e0cac325317e3c3f14b5e7f4ee09e49732
Ankit Pokhrel <ankitpokhrel@gmail.com>
2021-01-09 19:08:54 +0100

    chore(ci): Move linter script to makefile

--- scripts/lint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if ! command -v golangci-lint &>/dev/null; then
-  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b "$(go env GOPATH)/bin" v1.31.0
-fi
-
-golangci-lint run ./...
