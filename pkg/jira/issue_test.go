13672458f812d15539025a6a16a275f676abd96b
Ankit Pokhrel <ankitpokhrel@gmail.com>
2021-04-29 16:01:41 +0200

    Add tests

--- pkg/jira/issue_test.go
+++ pkg/jira/issue_test.go
@@ -158,3 +158,80 @@ func TestAssignIssue(t *testing.T) {
 	err = client.AssignIssue("TEST-1", "default")
 	assert.Error(t, ErrUnexpectedStatusCode, err)
 }
+
+func TestGetIssueLinkTypes(t *testing.T) {
+	var unexpectedStatusCode bool
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/rest/api/3/issueLinkType", r.URL.Path)
+
+		if unexpectedStatusCode {
+			w.WriteHeader(400)
+		} else {
+			resp, err := ioutil.ReadFile("./testdata/issue-link-types.json")
+			assert.NoError(t, err)
+
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(200)
+			_, _ = w.Write(resp)
+		}
+	}))
+	defer server.Close()
+
+	client := NewClient(Config{Server: server.URL}, WithTimeout(3))
+
+	actual, err := client.GetIssueLinkTypes()
+	assert.NoError(t, err)
+
+	expected := []*IssueLinkType{
+		{
+			ID:      "10000",
+			Name:    "Blocks",
+			Inward:  "is blocked by",
+			Outward: "blocks",
+		}, {
+			ID:      "10001",
+			Name:    "Cloners",
+			Inward:  "is cloned by",
+			Outward: "clones",
+		}, {
+			ID:      "10002",
+			Name:    "Relates",
+			Inward:  "relates to",
+			Outward: "relates to",
+		},
+	}
+	assert.Equal(t, expected, actual)
+
+	unexpectedStatusCode = true
+
+	_, err = client.GetIssueLinkTypes()
+	assert.Error(t, ErrUnexpectedStatusCode, err)
+}
+
+func TestLinkIssue(t *testing.T) {
+	var unexpectedStatusCode bool
+
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "/rest/api/3/issueLink", r.URL.Path)
+		assert.Equal(t, "application/json", r.Header.Get("Accept"))
+		assert.Equal(t, "application/json", r.Header.Get("Content-Type"))
+
+		if unexpectedStatusCode {
+			w.WriteHeader(400)
+		} else {
+			w.WriteHeader(201)
+		}
+	}))
+	defer server.Close()
+
+	client := NewClient(Config{Server: server.URL}, WithTimeout(3))
+
+	err := client.LinkIssue("TEST-1", "TEST-2", "Blocks")
+	assert.NoError(t, err)
+
+	unexpectedStatusCode = true
+
+	err = client.LinkIssue("TEST-1", "TEST-2", "invalid")
+	assert.Error(t, ErrUnexpectedStatusCode, err)
+}
