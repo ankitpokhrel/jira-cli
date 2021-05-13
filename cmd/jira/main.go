b88beae32221263131beb0ff9682ba6bdff78aa9
K <84071070+kmdpl@users.noreply.github.com>
2021-05-13 11:53:10 +0200

    Show help even if token is not set (#94)
    Co-authored-by: kmdpl <kmdpl@local>

--- cmd/jira/main.go
+++ cmd/jira/main.go
@@ -7,29 +7,10 @@ import (
 	"github.com/ankitpokhrel/jira-cli/internal/cmd/root"
 )
 
-const jiraAPITokenLink = "https://id.atlassian.com/manage-profile/security/api-tokens"
-
 func main() {
-	checkForJiraToken()
-
 	rootCmd := root.NewCmdRoot()
 	if _, err := rootCmd.ExecuteC(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 		os.Exit(1)
 	}
-}
-
-func checkForJiraToken() {
-	if os.Getenv("JIRA_API_TOKEN") != "" {
-		return
-	}
-
-	msg := fmt.Sprintf(`You need to define JIRA_API_TOKEN env for the tool to work. 
-
-You can generate a token using this link: %s
-
-After generating the token, export it to your shell and run 'jira init' if you haven't already.`, jiraAPITokenLink)
-
-	fmt.Fprintf(os.Stderr, "%s\n", msg)
-	os.Exit(1)
 }
