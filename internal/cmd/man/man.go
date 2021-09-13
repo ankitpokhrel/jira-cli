6494b0f54c4552dff4d3faf43cc7f727a28cc950
Santhosh Raju <santhosh.raju@gmail.com>
2021-09-14 00:43:47 +0200

    Refactor the man command into a single file.

--- internal/cmd/man/man.go
+++ internal/cmd/man/man.go
@@ -1,31 +1,64 @@
 package man
 
 import (
-	"github.com/spf13/cobra"
+	"os"
 
-	"github.com/ankitpokhrel/jira-cli/internal/cmd/man/generate"
+	"github.com/spf13/cobra"
+	"github.com/spf13/cobra/doc"
+)
+
+const (
+	examples = `# Generate man pages in default location /tmp/man-jira-cli
+$ jira man --generate
+
+# Generate man pages in specified location
+$ jira man --generate --output /path/to/man-pages`
 )
 
 // NewCmdMan is a man command.
 func NewCmdMan() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "man",
-		Short: "Help generate man(7) pages for Jira CLI.",
-		Long:  "Help generate man pages for Jira CLI compatible with UNIX style man pages.",
-		RunE:  man,
+		Use:     "man",
+		Short:   "Help generate man(7) pages for Jira CLI.",
+		Long:    "Help generate man pages for Jira CLI compatible with UNIX style man pages.",
+		Example: examples,
+		RunE:    man,
 	}
 
-	gm := generate.NewCmdGenerate()
-
-	cmd.AddCommand(
-		gm,
-	)
-
-	generate.SetFlags(gm)
+	cmd.Flags().BoolP("generate", "g", false, "Generate the man pages for Jira CLI")
+	cmd.Flags().StringP("output", "o", "/tmp/man-jira-cli", "Name of the directory where the man pages would be generated")
 
 	return &cmd
 }
 
 func man(cmd *cobra.Command, _ []string) error {
-	return cmd.Help()
+	gen, err := cmd.Flags().GetBool("generate")
+	if err != nil {
+		return err
+	}
+
+	if !gen {
+		return cmd.Help()
+	}
+
+	out, err := cmd.Flags().GetString("output")
+	if err != nil {
+		return err
+	}
+
+	// Create directory if it doesn't exist.
+	_, err = os.Stat(out)
+	if err != nil {
+		if err := os.MkdirAll(out, os.ModePerm); err != nil {
+			return err
+		}
+	}
+
+	header := &doc.GenManHeader{
+		Title:   "Jira CLI",
+		Section: "7",
+	}
+
+	// Generate man pages from the root.
+	return doc.GenManTree(cmd.Parent(), header, out)
 }
