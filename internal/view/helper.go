ankitpokhrel/jira-cli

commit 8204c1685599faaf10cf5334f60d3b89a1fdaf35

fix: Segmentation fault on view (#202)

internal/view/helper.go
@@ -146,3 +146,12 @@ func renderPlain(w io.Writer, data tui.TableData) error {
 	}
 	return nil
 }
+
+func getKeyColumnIndex(cols []string) int {
+	for i, col := range cols {
+		if col == fieldKey {
+			return i
+		}
+	}
+	return 1
+}

internal/view/epic.go
@@ -21,6 +21,7 @@ type EpicList struct {
 }
 
 // Render renders the epic explorer view.
+//nolint:dupl
 func (el EpicList) Render() error {
 	renderer, err := MDRenderer()
 	if err != nil {
@@ -36,7 +37,9 @@ func (el EpicList) Render() error {
 			tui.WithSelectedFunc(navigate(el.Server)),
 			tui.WithViewModeFunc(func(r, c int, d interface{}) (func() interface{}, func(interface{}) (string, error)) {
 				dataFn := func() interface{} {
-					issue, _ := api.ProxyGetIssue(api.Client(jira.Config{}), d.(tui.TableData)[r][1])
+					data := d.(tui.TableData)
+					ci := getKeyColumnIndex(data[0])
+					issue, _ := api.ProxyGetIssue(api.Client(jira.Config{}), data[r][ci])
 					return issue
 				}
 				renderFn := func(i interface{}) (string, error) {

internal/view/issues.go
@@ -59,7 +59,8 @@ func (l IssueList) Render() error {
 		tui.WithSelectedFunc(navigate(l.Server)),
 		tui.WithViewModeFunc(func(r, c int, _ interface{}) (func() interface{}, func(interface{}) (string, error)) {
 			dataFn := func() interface{} {
-				issue, _ := api.ProxyGetIssue(api.Client(jira.Config{}), data[r][1])
+				ci := getKeyColumnIndex(data[0])
+				issue, _ := api.ProxyGetIssue(api.Client(jira.Config{}), data[r][ci])
 				return issue
 			}
 			renderFn := func(i interface{}) (string, error) {
@@ -103,7 +104,10 @@ func (l IssueList) header() []string {
 		return validColumns[0:4]
 	}
 
-	var headers []string
+	var (
+		headers   []string
+		hasKeyCol bool
+	)
 
 	columnsMap := l.validColumnsMap()
 	for _, c := range l.Display.Columns {
@@ -111,6 +115,15 @@ func (l IssueList) header() []string {
 		if _, ok := columnsMap[c]; ok {
 			headers = append(headers, strings.ToUpper(c))
 		}
+		if c == fieldKey {
+			hasKeyCol = true
+		}
+	}
+
+	// Key field is required in TUI to fetch relevant data later.
+	// So, we will prepend the field if it is not available.
+	if !hasKeyCol {
+		headers = append([]string{fieldKey}, headers...)
 	}
 
 	return headers

internal/view/sprint.go
@@ -28,6 +28,7 @@ type SprintList struct {
 }
 
 // Render renders the sprint explorer view.
+//nolint:dupl
 func (sl SprintList) Render() error {
 	renderer, err := MDRenderer()
 	if err != nil {
@@ -47,7 +48,9 @@ func (sl SprintList) Render() error {
 			tui.WithSelectedFunc(navigate(sl.Server)),
 			tui.WithViewModeFunc(func(r, c int, d interface{}) (func() interface{}, func(interface{}) (string, error)) {
 				dataFn := func() interface{} {
-					issue, _ := api.ProxyGetIssue(api.Client(jira.Config{}), d.(tui.TableData)[r][1])
+					data := d.(tui.TableData)
+					ci := getKeyColumnIndex(data[0])
+					issue, _ := api.ProxyGetIssue(api.Client(jira.Config{}), data[r][ci])
 					return issue
 				}
 				renderFn := func(i interface{}) (string, error) {

commit d3527754700e61a945dbb7046418a4f6716d0ad8

fix: Copy issue introduced by #202

internal/view/helper.go
@@ -99,7 +99,7 @@ func issueKeyFromTuiData(r int, d interface{}) string {
 
 	switch data := d.(type) {
 	case tui.TableData:
-		path = data[r][1]
+		path = data[r][getKeyColumnIndex(data[0])]
 	case tui.PreviewData:
 		path = data.Key
 	}