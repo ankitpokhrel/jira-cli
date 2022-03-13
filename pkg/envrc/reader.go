37a8d2f181361cbc2311a64f18b972f1def69d9f
Adol Rodriguez <adolsalamanca@gmail.com>
2022-03-13 10:13:03 +0100

    add envrc reader

---
+++ pkg/envrc/reader.go
@@ -0,0 +1,104 @@
+package envrc
+
+import (
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+type netrcLine struct {
+	machine  string
+	login    string
+	password string
+}
+
+func readNetrc() ([]netrcLine, error) {
+	path, err := netrcPath()
+
+	if err != nil {
+		return []netrcLine{}, err
+	}
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return []netrcLine{}, err
+		}
+		return []netrcLine{}, err
+	}
+
+	netrc := parseNetrc(string(data))
+	return netrc, err
+}
+
+func netrcPath() (string, error) {
+	if env := os.Getenv("NETRC"); env != "" {
+		return env, nil
+	}
+
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	base := ".netrc"
+	if runtime.GOOS == "windows" {
+		base = "_netrc"
+	}
+	return filepath.Join(dir, base), nil
+}
+
+func parseNetrc(data string) []netrcLine {
+	var nrc []netrcLine
+	var l netrcLine
+	inMacro := false
+
+	for _, line := range strings.Split(data, "\n") {
+		if inMacro {
+			if line == "" {
+				inMacro = false
+			}
+			continue
+		}
+
+		f := strings.Fields(line)
+		i := 0
+
+		for ; i < len(f)-1; i += 2 {
+			// Reset at each "machine" token.
+			// “The auto-login process searches the .netrc file for a machine token
+			// that matches […]. Once a match is made, the subsequent .netrc tokens
+			// are processed, stopping when the end of file is reached or another
+			// machine or a default token is encountered.”
+
+			switch f[i] {
+			case "machine":
+				l = netrcLine{machine: f[i+1]}
+			case "default":
+				break
+			case "login":
+				l.login = f[i+1]
+			case "password":
+				l.password = f[i+1]
+			case "macdef":
+				// “A macro is defined with the specified name; its contents begin with
+				// the next .netrc line and continue until a null line (consecutive
+				// new-line characters) is encountered.”
+				inMacro = true
+			}
+
+			if l.machine != "" && l.login != "" && l.password != "" {
+				nrc = append(nrc, l)
+				l = netrcLine{}
+			}
+		}
+
+		if i < len(f) && f[i] == "default" {
+			// “There can be only one default token, and it must be after all machine tokens.”
+			break
+		}
+	}
+
+	return nrc
+}
---
+++ pkg/envrc/reader_test.go
@@ -0,0 +1,38 @@
+package envrc
+
+import (
+	"reflect"
+	"testing"
+)
+
+func Test_parseNetrc(t *testing.T) {
+	type args struct {
+		data string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []netrcLine
+	}{
+		{
+			name: "parses netrc file properly",
+			args: args{
+				data: "machine test.sample.org\nlogin mylogin@sample.org\npassword mypassword",
+			},
+			want: []netrcLine{
+				{
+					machine:  "test.sample.org",
+					login:    "mylogin@sample.org",
+					password: "mypassword",
+				},
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := parseNetrc(tt.args.data); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("parseNetrc() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
