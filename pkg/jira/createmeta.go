7e28b673f389adf09d6f11b56491d3381b1c6064
Ankit Pokhrel <oss@ankit.pl>
2022-08-20 14:43:08 +0200

    feat: Support for Jira v9

--- pkg/jira/createmeta.go
+++ pkg/jira/createmeta.go
@@ -23,6 +23,17 @@ type CreateMetaResponse struct {
 	} `json:"projects"`
 }
 
+// CreateMetaResponseJiraServerV9 struct holds response from GET /issue/createmeta endpoint
+// for jira server 9 and above.
+type CreateMetaResponseJiraServerV9 struct {
+	Values []struct {
+		ID      string `json:"id"`
+		Name    string `json:"name"`
+		Handle  string `json:"handle"`
+		Subtask bool   `json:"subtask"`
+	} `json:"values"`
+}
+
 // CreateMetaIssueType struct holds issue types from GET /issue/createmeta endpoint.
 type CreateMetaIssueType struct {
 	IssueType
@@ -53,6 +64,36 @@ func (c *Client) GetCreateMeta(req *CreateMetaRequest) (*CreateMetaResponse, err
 	}
 
 	var out CreateMetaResponse
+
+	err = json.NewDecoder(res.Body).Decode(&out)
+
+	return &out, err
+}
+
+// GetCreateMetaForJiraServerV9 gets create metadata using GET /issue/createmeta endpoint for jira server 9 and above.
+func (c *Client) GetCreateMetaForJiraServerV9(req *CreateMetaRequest) (*CreateMetaResponseJiraServerV9, error) {
+	path := fmt.Sprintf(
+		"/issue/createmeta/%s/issuetypes?expand=%s",
+		req.Projects, req.Expand,
+	)
+	if req.IssueTypeNames != "" {
+		path += fmt.Sprintf("&issuetypeNames=%s", req.IssueTypeNames)
+	}
+
+	res, err := c.GetV2(context.Background(), path, nil)
+	if err != nil {
+		return nil, err
+	}
+	if res == nil {
+		return nil, ErrEmptyResponse
+	}
+	defer func() { _ = res.Body.Close() }()
+
+	if res.StatusCode != http.StatusOK {
+		return nil, formatUnexpectedResponse(res)
+	}
+
+	var out CreateMetaResponseJiraServerV9
 
 	err = json.NewDecoder(res.Body).Decode(&out)
 
