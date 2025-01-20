7e1ccb79b046cf371f3a504aef07940de0ebbcdc
Sébastien <5331611+sebbbastien@users.noreply.github.com>
2025-01-20 11:48:52 +0000

    whitespace

--- internal/cmd/issue/create/create.go
+++ internal/cmd/issue/create/create.go
@@ -7,7 +7,7 @@ import (
 	"github.com/AlecAivazis/survey/v2"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
-	
+
 	"github.com/ankitpokhrel/jira-cli/api"
 	"github.com/ankitpokhrel/jira-cli/internal/cmdcommon"
 	"github.com/ankitpokhrel/jira-cli/internal/cmdutil"
