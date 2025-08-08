e9fd1f136183bb36da912aea4fb17373a8dd8b38
jmartasek <martasekja@lilly.com>
2025-08-08 14:41:11 +0000

    Fix lint 1

--- internal/cmd/api/api.go
+++ internal/cmd/api/api.go
@@ -44,9 +44,9 @@ $ jira api /rest/api/3/issue/PROJ-123 --translate-fields`
 // NewCmdAPI is an api command.
 func NewCmdAPI() *cobra.Command {
 	cmd := cobra.Command{
-		Use:   "api [endpoint]",
-		Short: "Make authenticated requests to the Jira API",
-		Long:  helpText,
+		Use:     "api [endpoint]",
+		Short:   "Make authenticated requests to the Jira API",
+		Long:    helpText,
 		Example: examples,
 		Annotations: map[string]string{
 			"cmd:main":  "true",
@@ -65,7 +65,7 @@ func NewCmdAPI() *cobra.Command {
 	return &cmd
 }
 
-// translateCustomFields replaces customfield_* IDs with their friendly names from config
+// translateCustomFields replaces customfield_* IDs with their friendly names from config.
 func translateCustomFields(data []byte, debug bool) []byte {
 	// Get the custom fields from the config
 	configuredFields, err := getCustomFieldsMapping()
@@ -86,7 +86,7 @@ func translateCustomFields(data []byte, debug bool) []byte {
 	}
 
 	// Try to detect any customfield_* patterns in the response that aren't in our config
-	var unrecognizedFields []string
+	unrecognizedFields := make([]string, 0)
 	re := regexp.MustCompile(`"(customfield_\d+)"`)
 	matches := re.FindAllStringSubmatch(string(data), -1)
 
@@ -135,7 +135,7 @@ func translateCustomFields(data []byte, debug bool) []byte {
 	return []byte(dataStr)
 }
 
-// getCustomFieldsMapping returns a map of custom field IDs to their friendly names
+// getCustomFieldsMapping returns a map of custom field IDs to their friendly names.
 func getCustomFieldsMapping() (map[string]string, error) {
 	var configuredFields []jira.IssueTypeField
 
