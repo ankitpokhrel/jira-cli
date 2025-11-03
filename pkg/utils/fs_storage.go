5011acde9452477bcd868e6c7311e264e84a25cb
Christian Arty <40256027+christianarty@users.noreply.github.com>
2025-11-02 20:36:45 -0500

    reorganize the storage and filesystem tests

---
+++ pkg/utils/fs_storage.go
@@ -0,0 +1,32 @@
+package utils
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const (
+	OWNER_ONLY       = 0o700
+	OWNER_READ_WRITE = 0o600
+)
+
+// FileSystemStorage implements Storage interface for filesystem operations.
+type FileSystemStorage struct {
+	// BaseDir is the directory where the storage will be saved
+	BaseDir string
+}
+
+func (fs FileSystemStorage) Save(key string, value []byte) error {
+	if err := os.MkdirAll(fs.BaseDir, OWNER_ONLY); err != nil {
+		return fmt.Errorf("failed to create directory: %w", err)
+	}
+
+	filePath := filepath.Join(fs.BaseDir, key)
+	return os.WriteFile(filePath, value, OWNER_READ_WRITE)
+}
+
+func (fs FileSystemStorage) Load(key string) ([]byte, error) {
+	filePath := filepath.Join(fs.BaseDir, key)
+	return os.ReadFile(filePath)
+}
